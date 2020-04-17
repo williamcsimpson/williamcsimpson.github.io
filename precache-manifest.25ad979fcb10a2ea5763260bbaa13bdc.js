e5776680c522692293299461bb6f4a0f0669bfd7
William <wcsimpso@ucsd.edu>
2020-04-17 13:13:55 -0700

    Re-Added Monoalphabetic Breaker

---
+++ precache-manifest.25ad979fcb10a2ea5763260bbaa13bdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0711a4f3985842c5079f0af128e97e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53573834a868fd255e02",
+    "url": "/static/js/2.6eed52ef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6eed52ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee8b42c72268e7ec977e",
+    "url": "/static/js/main.a880d057.chunk.js"
+  },
+  {
+    "revision": "83529cbfb27924d638ec",
+    "url": "/static/js/runtime-main.1ef1dd5e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b1151c2d1bfdf061cb55b6b606e79765.js"
+  "/precache-manifest.25ad979fcb10a2ea5763260bbaa13bdc.js"
 );
 
 self.addEventListener('message', (event) => {
